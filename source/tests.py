8d5bc5db4b37b41ccbd18f5441704c96d16b6af1
admin <zuevdaniil.zuevdaniil@gmail.com>
2022-01-01 21:26:09 +0300

    added folders, made nice README.md illustration

--- source/tests.py
+++ source/tests.py
@@ -253,12 +253,12 @@ class ReadMeExampleTests(unittest.TestCase):
     def test_t1(self):
         from time import time
         import asyncio
-        import task_graph as tg
+        import task_graph as tg  # source/task_graph.py
 
         # set up some dummy async functions:
-        async def str_to_int(x: str) -> int:
-            await asyncio.sleep(10)  # conversion to int is really complex :\
-            return int(x)
+        async def str_to_complex(x: str) -> int:
+            await asyncio.sleep(10)  # conversion to complex is really complex :\
+            return complex(x)
 
         async def str_to_float(x: str) -> float:
             await asyncio.sleep(3)  # float is much easier
@@ -266,7 +266,7 @@ class ReadMeExampleTests(unittest.TestCase):
 
         async def linear_complexity(x: float) -> float:
             await asyncio.sleep(x)
-            return x
+            return x * 2
 
         async def bad_task(x) -> None:
             raise Exception("error occurred mid-task")
@@ -276,12 +276,12 @@ class ReadMeExampleTests(unittest.TestCase):
 
         # now build the graph:
         start_node = tg.NoOp(tg.Result.lift("2.0"))
-        to_int_node = start_node >= tg.Func(str_to_int)
+        to_complex_node = start_node >= tg.Func(str_to_complex)
         # `>=` is a "do_after" operator for tg.Operation subclasses. returns second operand
         to_float_node = start_node >= tg.Func(str_to_float)  # parallel with to_int_node
-        gather = tg.GatherN([to_int_node, to_float_node], n=1)  # wait for one task to finish
+        gather = tg.GatherN([to_complex_node, to_float_node], n=1)  # wait for one task to finish
         unwrap_result_node: tg.Func = gather >= tg.Func(gather_unwrap)
-        bad_node = unwrap_result_node >= NoOp(tg.Result.lift("any").map(bad_task))  # will immediately fail
+        bad_node = unwrap_result_node >= tg.NoOp(tg.Result.lift("any").map(bad_task))  # will immediately fail
         lin_complexity_node = unwrap_result_node | linear_complexity
         lin_complexity_node >= tg.TERM_OP  # TERM_OP is a termination signal
 
@@ -299,13 +299,13 @@ class ReadMeExampleTests(unittest.TestCase):
 
         # examine the results
         print("execution time: ", execution_time)
-        assert (~~lin_complexity_node_) == 2.0
+        assert (~~lin_complexity_node_) == 2.0 * 2
         print(f"{bad_node} result was {repr(bad_node.result.box)}")  # this was bound to fail, remember?
         try:
-            ~~to_int_node  # trying to unwrap it will raise an error,
+            ~~to_complex_node  # trying to unwrap it will raise an error,
             # since task takes 10 s but the TERM_OP was encountered in ~ 5s
         except asyncio.futures.CancelledError as e:
-            print(f"{to_int_node} was cancelled")
+            print(f"{to_complex_node} was cancelled")
 
 
 if __name__ == "__main__":
